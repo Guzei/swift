5544725a7bf58a9c582ac1627cbb90c899114327
Guzei <igor@guzei.com>
2022-10-25 23:58:35 +0300

    ver.2

--- inheritance.playground/Contents.swift
+++ inheritance.playground/Contents.swift
@@ -3,6 +3,15 @@
 //
 // https://docs.swift.org/swift-book/LanguageGuide/Inheritance.html
 // https://swiftbook.ru/content/languageguide/inheritance/
+// https://youtu.be/1D-DFsNS1tY - MadiSpace - 22 дек. 2021 г. - чуть больше минимума
+// https://youtu.be/70wPY4QbSzY - Ian Solomein - 17 июл. 2020 г. - необходимый минимум
+// https://youtu.be/ldWWugGV1fg28 - The Swift Developers (Evgeny Zverik) - 28 февр. 2017 г. - интерфейс устраревает, но суть остаётся
+// https://youtu.be/PWDx6z0wnoY - Alex Skutarenko - 17 мар. 2015 г.
+
+
+
+
+
 
 class   BaseClass{}
 class    SubClass: BaseClass{}
@@ -25,9 +34,10 @@ class SubClass3: BaseClass3{override var a: Int{return super.a + 1}}
 
 // Metods
 class BaseClass4{func foo(){}}
-class SubClass4: BaseClass4{override func foo(){super.foo()};func foo5(){}}
+final class SubClass4: BaseClass4{override func foo(){super.foo()};func foo5(){}}
+// class ErrClass: SubClass4{} // Inheritance from a final class ...
 
-// init
+// https://swiftbook.ru/content/languageguide/initialization/
 // designated // назначенный // обязательный
 // convenience // удобство, преимущество // необязательный
 // инициализаторы обязательно должны ссылаться друг на друга вплодь до верхнего класса
@@ -37,18 +47,36 @@ class BaseClass5 {
         self.a = a
     }
 }
-final class SubClass5: BaseClass5 {
-    override init(a:Int){
+
+class SubClass5: BaseClass5 {
+    override init(a:Int) {
         super.init(a:a)
     }
-    convenience init(b: Int, c: Int) {
-        self.init(a: b * c)
+    convenience init(_ bb: Int, _ cc: Int) {
+        self.init(a: bb * cc)
     }
 }
 var a: SubClass5
-a = SubClass5(a: 2) // override.init
+a = SubClass5(a: 2) // designated.init
 a.a // 2
-a = SubClass5(b: 3, c: 5) // convenience.init
+a = SubClass5(3, 5) // convenience.init
 a.a // 15
 
-// class ErrClass: SubClass5{} // Inheritance from a final class 'SubClass5'
+class SubClass51: BaseClass5 {
+    var b:Int
+    init(a:Int,b:Int) {
+        self.b = b
+        super.init(a:a)
+    }
+    convenience init(_ bb: Int, _ cc: Int, _ dd: Int) {
+        self.init(a: bb * cc, b: dd)
+    }
+}
+var b: SubClass51
+b = SubClass51(a: 2, b: 3) // designated.init
+b.a // 2
+b.b // 3
+b = SubClass51(3, 5, 2) // convenience.init
+b.a // 15
+b.b // 2
+
